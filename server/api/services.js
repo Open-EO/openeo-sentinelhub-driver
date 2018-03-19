0537f1513b2660013dbf386e599c0b74c23a0927
Matthias Mohr <webmaster@mamo-net.de>
2018-03-19 14:46:32 +0100

    Sending default service_args

--- server/api/services.js
+++ server/api/services.js
@@ -3,29 +3,37 @@ const { createJobCacheKey, createServiceCacheKey, getWmsUrl } = require('./util'
 function services_post (req, res, next) {
   try {
     serviceInput = req.body;
-	
-	if (typeof serviceInput.job_id === 'undefined') {
-		throw "No job_id specified.";
-	}
-	if (typeof serviceInput.service_type !== 'string' && serviceInput.service_type === 'wms') {
-		throw "Only service_type 'wms' is supported.";
-	}
-	
-	var job = req.storage.get(createJobCacheKey(serviceInput.job_id));
-	if (!job) {
-		throw "No job found with the specified id.";
-	}
+
+    if (typeof serviceInput.job_id === 'undefined') {
+      throw "No job_id specified.";
+    }
+    if (typeof serviceInput.service_type !== 'string' && serviceInput.service_type === 'wms') {
+      throw "Only service_type 'wms' is supported.";
+    }
+
+    var job = req.storage.get(createJobCacheKey(serviceInput.job_id));
+    if (!job) {
+      throw "No job found with the specified id."
+    }
 
     const uuid = require('node-uuid').v1()
     req.storage.set(createServiceCacheKey(uuid), serviceInput)
-	
-	console.log("created service (" + serviceInput.service_type + ") with id: " + uuid);
+
+    console.log("created service (" + serviceInput.service_type + ") with id: " + uuid);
 
     res.json({
       service_id: uuid,
       service_url: getWmsUrl(req, uuid),
-	  service_type: serviceInput.service_type,
-      service_args: {},
+      service_type: serviceInput.service_type,
+      service_args: {
+        name: 'WMS',
+        request: 'GetCoverage',
+        service: 'WMS',
+        coverage: 'CUSTOM',
+        format: 'image/jpeg',
+        tileSize: 256,
+        minZoom: 8
+      },
       job_id: serviceInput.job_id
     })
   } catch (e) {
@@ -36,10 +44,10 @@ function services_post (req, res, next) {
 }
 
 function services_delete(req, res, next) {
-	req.storage.expire(createServiceCacheKey(req.params.service_id));
-	console.log("deleted service with id: " + req.params.service_id);
-	res.send(200);
-	return next();
+  req.storage.expire(createServiceCacheKey(req.params.service_id));
+  console.log("deleted service with id: " + req.params.service_id);
+  res.send(200);
+  return next();
 }
 
 module.exports = {
